b658c10953233e8ce51b6368807405c48a3856e2
Imelda1005 <zahrahimeldaasari@gmail.com>
2026-06-17 22:50:17 +0700

    tryy

--- database.php
+++ database.php
@@ -10,28 +10,28 @@ class Database {
         $this->conn = null;
 
         try {
-            // Tambah SSL dan timeout options
+            // PDO options dengan SSL
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                // SSL Configuration untuk Azure MySQL
+                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                PDO::ATTR_TIMEOUT => 10
             ];
             
-            // Coba dengan username format "dbadmin@mysql-praktikumsubmit"
+            // DSN connection string
             $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name;
             
             $this->conn = new PDO($dsn, $this->username, $this->password, $options);
             
         } catch(PDOException $exception) {
-            // Tampilkan error detail (untuk debugging)
+            // Tampilkan error detail
             die("Connection error: " . $exception->getMessage() . "<br><br>
                  <b>Debug Info:</b><br>
                  Host: " . $this->host . "<br>
                  Database: " . $this->db_name . "<br>
-                 Username: " . $this->username . "<br>
-                 Error Code: " . $exception->getCode());
+                 Username: " . $this->username);
             return null;
         }
 
